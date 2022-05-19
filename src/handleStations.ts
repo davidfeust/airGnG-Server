9e81ab0812f75d0009c14bc7886e591474e25779
achiyazigi <matanelcohen0@walla.com>
2022-05-19 22:12:32 +0300

    delete one station

--- src/handleStations.ts
+++ src/handleStations.ts
@@ -1,7 +1,8 @@
 // (/stations)
 import express, { Request } from 'express';
-import { doc, getDoc } from 'firebase/firestore';
-import { db } from '../config/firebase';
+import { deleteDoc, doc, getDoc } from 'firebase/firestore';
+import { deleteObject, ref } from 'firebase/storage';
+import { db, storage } from '../config/firebase';
 import { Station } from './index.d';
 import { getFromCol } from './utils/GlobalFunctions';
 
@@ -20,6 +21,27 @@ router.get('/', async (req, res) => {
         stations = map;
     });
     res.json(stations);
+});
+
+router.delete('/:id', async (req: Request<{ id: string }>, res) => {
+    // tslint:disable-next-line:no-console
+    console.log('got request for DELETE stations/' + req.params.id);
+    const docRef = doc(db, 'stations', req.params.id);
+
+    deleteDoc(docRef).catch((err) => {
+        res.status(500).send(err);
+    });
+
+    deleteObject(
+        ref(
+            storage,
+            `gs://airgng-dfc98.appspot.com/images_stations/${req.params.id}.jpg`
+        )
+    )
+        .then(() => res.sendStatus(200))
+        .catch((err) => {
+            res.status(500).send(err);
+        });
 });
 
 router.get('/:id', async (req: Request<{ id: string }>, res) => {
